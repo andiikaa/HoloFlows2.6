e3bb409b43f3c839a69207ca9001ec53724ec170
andiikaa <andre.kuehnie@gmail.com>
2019-03-15 13:12:12 +0100

    basic settings

---
+++ HoloFlows2.6/Assets/HoloFlows/Scripts/Style.cs
@@ -0,0 +1,42 @@
+﻿using HoloToolkit.Unity;
+using UnityEngine;
+
+public class Style : Singleton<Style>
+{
+
+    public Material highlightMat;
+    public Color highlightColor;
+    public Color defaultColor;
+    public Material ClickedOnMat;
+    public Color ClickedOnColor;
+
+    public Material LineMaterialWFMode;
+    public Material LineMaterialNotWFMode;
+    public Material ArrowMaterialWFMode;
+    public Material ArrowMaterialNotWFMode;
+
+    public Material AndWFMode;
+    public Material OrWFMode;
+
+    public Material AndChosen;
+    public Material OrChosen;
+
+    public Material AndNotWFMode;
+    public Material OrNotWfMode;
+
+    public Material SimpleBlue;
+    public Material Invisible;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
