3f84bc0b2ffa0040c09ac8646f353f95c58433b2
andiikaa <andre.kuehnie@gmail.com>
2019-05-08 17:19:09 +0200

    requests for sending commands

---
+++ HoloFlows2.6/Assets/HoloFlows/Scripts/Client/ItemCommandRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine.Networking;
+
+namespace HoloFlows.Client
+{
+    class ItemCommandRequest : PostRequestBase
+    {
+        private readonly Action<bool> requestDone;
+        private const string PLAIN_CONTENT_TYPE = "text/plain";
+        private const string URI_TARGET = "rest/items/{0}";
+
+
+        public ItemCommandRequest(string requestURL, string itemId, Action<bool> requestDone = null)
+            : base(requestURL, string.Format(URI_TARGET, itemId), PLAIN_CONTENT_TYPE)
+        {
+            this.requestDone = requestDone;
+        }
+
+        protected override void HandleResponse(UnityWebRequest www)
+        {
+            requestDone?.Invoke(true);
+        }
+    }
+}
---
+++ HoloFlows2.6/Assets/HoloFlows/Scripts/Settings.cs
@@ -0,0 +1,9 @@
+﻿namespace HoloFlows
+{
+    public class Settings
+    {
+        public const string OPENHAB_URI = "http://192.168.1.3:8080";
+        public const string UNKNOWN_STATE = "-";
+        public const int POLLING_DELAY_FRAMES = 11;
+    }
+}
