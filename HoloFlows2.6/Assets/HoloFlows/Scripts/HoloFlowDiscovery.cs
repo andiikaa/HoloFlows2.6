4f842552ce4265dc0b76eb1f3efdfaf4bfc421c6
andiikaa <andre.kuehnie@gmail.com>
2019-06-12 19:16:24 +0200

    added mdns lib - but does not work :(

---
+++ HoloFlows2.6/Assets/HoloFlows/Scripts/HoloFlowDiscovery.cs
@@ -0,0 +1,44 @@
+﻿using Tmds.MDns;
+using UnityEngine;
+
+namespace HoloFlows
+{
+    //FIXME: will not work within unity
+    //fails at runtime
+    public class HoloFlowDiscovery : MonoBehaviour
+    {
+        public const string proteusMdnsName = "_proteus._tcp";
+        public const string openhabMdnsName = "_openhab-server._tcp";
+
+        public void Start()
+        {
+            string[] serviceTypes = { proteusMdnsName, openhabMdnsName };
+
+            ServiceBrowser serviceBrowser = new ServiceBrowser();
+            serviceBrowser.ServiceAdded += onServiceAdded;
+            serviceBrowser.ServiceRemoved += onServiceRemoved;
+            serviceBrowser.ServiceChanged += onServiceChanged;
+            serviceBrowser.StartBrowse(serviceTypes);
+        }
+
+        static void onServiceChanged(object sender, ServiceAnnouncementEventArgs e)
+        {
+            printService('~', e.Announcement);
+        }
+
+        static void onServiceRemoved(object sender, ServiceAnnouncementEventArgs e)
+        {
+            printService('-', e.Announcement);
+        }
+
+        static void onServiceAdded(object sender, ServiceAnnouncementEventArgs e)
+        {
+            printService('+', e.Announcement);
+        }
+
+        static void printService(char startChar, ServiceAnnouncement service)
+        {
+            Debug.LogFormat("{2} Found: {0} on {1}", service.Type, string.Join(", ", service.Addresses), startChar);
+        }
+    }
+}
